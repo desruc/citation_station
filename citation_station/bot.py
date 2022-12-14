de39ff4816cb3f5efc5796690953972a66781185
James Cameron <36631337+desruc@users.noreply.github.com>
2022-12-15 07:50:11 +1000

    fix: remove old ext functions

--- citation_station/bot.py
+++ citation_station/bot.py
@@ -3,26 +3,16 @@ from discord.ext import commands
 
 from citation_station.scheduler.scheduler import Scheduler
 
+
 class Bot(commands.Bot):
     def __init__(self, *args, **kwargs):
         self.scheduler = Scheduler(self)
-
         super().__init__(*args, **kwargs)
-
-    async def load_exts(self):
-        for filename in os.listdir('./citation_station/exts'):
-            if filename.endswith('.py'):
-                try:
-                    await self.load_extension(f'citation_station.exts.{filename[:-3]}')
-                    print(f"Loaded extension '{filename}'")
-                except Exception as e:
-                    print(f"Failed to load extension {filename}. Error: {e}")
 
     @commands.Cog.listener()
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         self.scheduler.initialize()
-
 
     @commands.Cog.listener()
     async def on_message(self, message):
